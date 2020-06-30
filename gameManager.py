401a3a07d62c917709f77d7eca856040e774dc42
Taras Didukh <botanichek2015@gmail.com>
2020-06-30 11:04:32 +0300

    merge with server

--- gameManager.py
+++ gameManager.py
@@ -2,6 +2,57 @@ import random
 import string
 import time
 
+import socketio
+
+# sio = socketio.Client()
+
+
+# @sio._handle_error(namespace='/battle', data='')
+# def on_error(err):
+#     print("error")
+# @sio.on('connect', namespace='/battle')
+# def on_connect():
+#     print("I'm connected to the /battle namespace!")
+#     sio.emit("message", "players")
+
+# @sio.event
+# def disconnect():
+#     print("I'm disconnected")
+
+# @sio.event
+# def connect_error(err):
+#     print("The connection failed!")
+    
+# @sio.event
+# def players(players):
+#     print("here")
+#     print(players)
+#     num = int(input("chose to play: "))
+#     if num == -1:
+#         return
+#     sio.emit("play_with", players[num][0])
+
+# @sio.event
+# def ask_play(player):
+#     print(player["player"] + " wants to play with you.")
+#     input("type anything to start ")
+#     sio.emit("accept", player["id"])
+
+
+# @sio.on("message", namespace='/battle')
+# def on_message(message):
+#     print("message")
+
+
+
+# try:
+#     sio.connect('http://localhost:5000?username=Taras', namespaces=['/battle'])
+# except socketio.exceptions.ConnectionError:
+#     print("error")
+# print('my sid is: ', sio.sid)
+
+
+
 WIN_STOOTS = 60
 
 class ShootType:
@@ -10,13 +61,61 @@ class ShootType:
     KILL = "вбив корабель"
     MISS = "мимо"
 
+sio = socketio.Client()
 
-class GameManager:
+class GameManager(socketio.ClientNamespace):
     __shoots = 0
     
-    def shoot(self, location):
+
+    def __init__(self, on_shoot, shot_answer_from, namespace):
+        socketio.ClientNamespace.__init__(self, namespace)
+        
+        self.on_shoot = on_shoot
+        self.shot_answer_from = shot_answer_from
+        
+    def on_connect(self):
+        print("connected")
+        self.emit("message", "players")
+
+    def disconnect(self):
+        print("I'm disconnected")
+    
+    def on_players(self, players):
+        print(players)
+        num = input("вибери з ким грати: ")
+        if num.isnumeric():
+            num = int(num)
+            if num < len(players):
+                print("Почекай з'єднання...")
+                self.opponent = players[num][0]
+                self.emit("play_with", self.opponent)
+                return
+        print("Чекайте поки Вас виберуть або перезапустіть.")
+
+    def on_ask_play(self, player):
+        print(player["player"] + " хоче грати з тобою")
+        input("Напиши щось і починаєм > ")
+        self.opponent = player["id"]
+        self.emit("accept", self.opponent)
+
+    def on_start(self, player_id):
+        self.on_shoot("")
+    
+    def shoot_to(self, location):
         self.__shoots += 1
-        return ShootType.WIN_GAME if self.__shoots > WIN_STOOTS else random.choice([ShootType.HIT, ShootType.MISS, ShootType.KILL])
+        self.emit("shot", {"player": self.opponent, "zone": location})
+        #return ShootType.WIN_GAME if self.__shoots > WIN_STOOTS else random.choice([ShootType.HIT, ShootType.MISS, ShootType.KILL])
+    def on_shot_answer(self, data):
+        self.shot_answer_from(data)
+    
+    def shot_answer_to(self, answer):
+        answer["player"] = self.opponent
+        self.emit("shot_answer", answer)
+
+    def on_shot(self, data):
+        self.on_shoot(data["zone"])
+    
+
 
     def opponent_shoot(self):
         print("...Почекай, я подумаю...")
--- main.py
+++ main.py
@@ -12,7 +12,7 @@ class Zone :
     self.type=type
 class ZoneType :
   SPACE=" "
-  MISS="*"
+  MISS="•"
   SHIP="#"
   HIT="x"
     
@@ -105,7 +105,7 @@ def fill_random():
             pass
 
 
-def printField(f):
+def printField(f, retrieve = False):
     def convert(z): 
         return  z.type #str(z.x) + "-" + str(z.y)
     
@@ -114,7 +114,8 @@ def printField(f):
     resultShip = alphabet
     for i in range(0, size):
         resultShip = resultShip + "{:02d}".format(i + 1) + " " + "|".join(map(convert,f[i])) + "\n"
-    print(resultShip)
+    if not retrieve:
+        print(resultShip)
     
     return resultShip
     
@@ -141,31 +142,52 @@ game_file_opponent.write( printField(opponent_field) )
 game_file.close()
 game_file_opponent.close()
 
-manager = GameManager()
+manager = None 
 alphabet = "ABCDEFGHJK"
 
 def save_current_state():
     game_file_current = open(os.path.join(path_original_field, "my_current.txt"), "wt")
-    game_file_current.write(printField(field))
+    game_file_current.write(printField(field, True))
     game_file_current.close()
 
+def save_opponent_state():
+    game_file_opponent = open(os.path.join(path_original_field, "opponent.txt"), "wt")
+    game_file_opponent.write(printField(opponent_field, True))
+    game_file_opponent.close()
+
+def shot_answer_from(data):
+    location = data["zone"]
+    result = data["answer"]
+    parsed = parse(location)
+    zone = opponent_field[parsed["y"]][parsed["x"]]
+    
+    
+    if result == ShootType.MISS:
+        zone.type = ZoneType.MISS
+        save_opponent_state()
+        print("Я промахнувся")
+    elif result == ShootType.WIN_GAME:
+        zone.type = ZoneType.HIT
+        save_opponent_state()
+        print("Ура, я Виграв!")
+    elif result == ShootType.KILL:
+        zone.type = ZoneType.HIT
+        save_opponent_state()
+        print("Кул, я потопив корабель!")
+        move()
+    else:
+        zone.type = ZoneType.HIT
+        save_opponent_state()
+        print("Єс, попав.")
+        move()
+
 def move():
-    save_current_state()
-    printField(field)
+    printField(opponent_field)
     location = input("Стріляю: ")
     if location == ".":
         print("Кінець")
         return True
-    result = manager.shoot(location)
-    
-    if result == ShootType.MISS:
-        print("Я промахнувся")
-        opponent()
-    elif result == ShootType.WIN_GAME:
-        print("Ура, я Виграв!")
-    else:
-        print("Єс, попав.")
-        move()
+    manager.shoot_to(location)
     
 def is_destroyed():
     for i in range(0, size):
@@ -174,27 +196,123 @@ def is_destroyed():
                 return False  
     return True 
 
-def opponent():
-    save_current_state()
-    location = manager.opponent_shoot()
-    print("location: " + location[0] + str(int(location[1]) + 1) )
-    zone = field[int(location[1])][alphabet.find(location[0])]
+def check(x, y):
+    if field[y][x].type in [ZoneType.MISS, ZoneType.SPACE]:
+        return 0
+    if field[y][x].type == ZoneType.SHIP:
+        return 1
+    return -1
+
+def is_ship_destroyed(zone):
+    for i in range(zone.x, -1, -1):
+        res = check(i, zone.y)
+        if res == 0:
+            break
+        elif res == 1:
+            return False
+
+    for i in range(zone.x, len(field)):
+        res = check(i, zone.y)
+        if res == 0:
+            break
+        elif res == 1:
+            return False
+            
+    for i in range(zone.y, len(field[0])):
+        res = check(zone.x, i)
+        if res == 0:
+            break
+        elif res == 1:
+            return False
+
+    for i in range(zone.y, -1, -1):
+        res = check(zone.x, i)
+        if res == 0:
+            break
+        elif res == 1:
+            return False
+    
+    return True
+
+def parse(location):
+    x = None
+    y = None
+    if location[0:2].isnumeric():
+        y = int(location[0:2])
+        x = alphabet.find(location[2])
+    elif location[0].isnumeric():
+        y = int(location[0])
+        x = alphabet.find(location[1])
+    elif location[1:3].isnumeric():
+        y = int(location[1:3])
+        x = alphabet.find(location[0])
+    elif location[1].isnumeric():
+        y = int(location[1])
+        x = alphabet.find(location[0])
+
+    if x == None or y == None:
+        raise IndexError()
+    return {"x": x, "y": y - 1}
+
+
+
+def opponent(location):
+    if location == "":
+        move()
+        return
+    # location = manager.opponent_shoot()
+    zone = None
+    try:
+        print("Мені стріляли: " + location)
+        parsed = parse(location)
+        zone = field[parsed["y"]][parsed["x"]]
+    except:
+        print("Противник написав: " + location)
+        move()
+        return
+    
     if zone.type == ZoneType.SHIP:
         zone.type = ZoneType.HIT
-        print("Нас підстрелили!")
+        save_current_state()
+        printField(field)
         if is_destroyed():
             print("Я програв!")
+            manager.shot_answer_to({"zone": location, "answer": ShootType.WIN_GAME})
             return
-        opponent()
+        if is_ship_destroyed(zone):
+            print("Мій корабель знищений!")
+            manager.shot_answer_to({"zone": location, "answer": ShootType.KILL})
+            return
+        print("Нас підстрелили!")
+        manager.shot_answer_to({"zone": location, "answer": ShootType.HIT})
     elif zone.type in [ZoneType.SPACE, ZoneType.MISS]:
-        print("Противник промахнувся!")
         zone.type = ZoneType.MISS
+        save_current_state()
+        printField(field)
+        print("Противник промахнувся!")
+        manager.shot_answer_to({"zone": location, "answer": ShootType.MISS})
         move()
     else:
         print("Противник вже стріляв!")
         move()
+
+manager = GameManager(opponent,shot_answer_from, "/battle")
+sio.register_namespace(manager)
+my_name = input("Напишіть своє ім'я: ")
+try:
+    
+    server = "https://battle-ship-cc.herokuapp.com/"
+    #server = "http://0.0.0.0:5000" # "http://localhost:5000"
+    sio.connect(server + "?username=" + my_name, namespaces=['/battle'])
+except:
+    print("error")
+
+sio.wait()
+# sio.sleep(5000)
+# sio.sleep(100)
 #printField()
-move()
+#move()
+
 
 
 
